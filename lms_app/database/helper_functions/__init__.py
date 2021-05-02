71b1837fa07a97a6fd0dc919922ee480dd6af8e3
Mehul Vaghani <mrvaghani@users.noreply.github.com>
2021-05-02 15:39:22 -0400

    Added some NULL values to make the data more realistic

--- lms_app/database/helper_functions/__init__.py
+++ lms_app/database/helper_functions/__init__.py
@@ -1,11 +1,38 @@
+import datetime
+
 import pyodbc
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=server_name;'
-                      'Database=database_name;'
-                      'Trusted_Connection=yes;')
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv, find_dotenv
 
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM database_name.table')
+dotenv_path = join(os.path.realpath('../../ENV'), 'SQL_SERVER.env')
+# dotenv_path = find_dotenv('SQL_SERVER.env')
+load_dotenv(dotenv_path)
 
-for row in cursor:
-    print(row)
+sql_server = 'localhost'
+sql_database = 'CS631'
+sql_username = os.environ.get("MSSQL_SA_USER")
+sql_password = os.environ.get("MSSQL_SA_PASSWORD")
+
+cnxn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={sql_server};DATABASE={sql_database};UID={sql_username};PWD={sql_password}')
+cursor = cnxn .cursor()
+
+# Sample select query
+cursor.execute("SELECT * FROM CS631.dbo.document;")
+row = cursor.fetchone()
+while row:
+    print(f'{row[0]}\t {row[1]}')
+    row = cursor.fetchone()
+
+
+# cnxn2 = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={sql_server};DATABASE=Testdb;UID={sql_username};PWD={sql_password}')
+# cursor2 = cnxn2 .cursor()
+# # Sample insert query
+# # https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver15
+# CURRENT_TIMESTAMP = datetime.datetime.now()
+# count = cursor.execute("""
+# INSERT INTO Testdb.dbo.sample (firstname, lastname, age, dob)
+# VALUES (?,?,?,?)""",
+# 'John2', 'Cena', 50, '01/20/2020').rowcount
+# cnxn.commit()
+# print('Rows inserted: ' + str(count))
